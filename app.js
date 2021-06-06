11e21a74381c3a8826c381f3e169c04bcbcc911c
Chris <chrisdarnell@gmail.com>
2021-06-06 15:15:10 -0500

    Work Page Added

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const express = require("express");
 
 const app = express();
 
-let itemsToAdd = [];
+let items = [];
+let workItems = [];
 
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.static("public"));
 
-
 app.set("view engine", "ejs");
+
+// Template
 
 app.get("/", (req, res) => {
     let today = new Date();
@@ -25,19 +27,36 @@ app.get("/", (req, res) => {
     let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
-        kindOfDay: day,
-        newListItems: itemsToAdd
+        listTitle: day,
+        newListItems: items
     });
 });
 
 app.post("/", (req, res) => {
-    itemToAdd = req.body.newItem;
-    itemsToAdd.push(itemToAdd);
-
-    res.redirect("/");
+    let item = req.body.newItem;
+    let testVar = req.body.list;
+    console.log(testVar);
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
 
+// Work Page 
+
+app.get("/work", (req, res) => {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+});
+
+
+// Listen
 
 app.listen(3000, () => {
     console.log("Server started port 3000");
