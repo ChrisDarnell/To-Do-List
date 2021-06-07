6afc989b4037797b52dd9b2dfef25702b3999bad
Chris <chrisdarnell@gmail.com>
2021-06-06 19:54:52 -0500

    Refactor - Complete date.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //jshint esversion:6
 
 const express = require("express");
+const date = require(__dirname +"/date.js");
+
+
 
 const app = express();
 
@@ -17,14 +20,8 @@ app.set("view engine", "ejs");
 // Template
 
 app.get("/", (req, res) => {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
 
-    let day = today.toLocaleDateString("en-US", options);
+    let day = date();
 
     res.render("list", {
         listTitle: day,
